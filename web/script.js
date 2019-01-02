ab63cf845f399e4441d3eeea55edaea0f1905781
Steven Davies <github@steev.me.uk>
2019-01-02 20:26:18 +0000

    Add web files to source control

---
+++ web/script.js
@@ -0,0 +1,124 @@
+var overrideTemp = 0.0;
+
+function getNewValues( immediate, initial ) {
+  var url = "/heating/all_details";
+  if( immediate ) {
+    url = url + "/no_wait";
+  }
+  var qry = $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "json",
+    cache: false
+  })
+    .done( function( data ) {
+      processNewValues( data );
+      if ( initial )
+        setTimeout( function() { getNewValues( false, initial )}, 5000 );
+    });
+}
+
+function processNewValues( data ) {
+  for ( var key in data.temps ) {
+    $("#"+key+"temp").html( data.temps[key].toFixed(1) + "&deg;" );
+  }
+
+  if ( data.heating ) {
+    $( "#heatingstatus" ).removeClass( "bgwhite" ).addClass( "bgred" ).text( "Heating: On" );
+  } else {
+    $( "#heatingstatus" ).removeClass( "bgred" ).addClass( "bgwhite" ).text( "Heating: Off" );
+  }
+
+  if ( data.preheat ) {
+    $( "#preheatstatus" ).removeClass( "bgwhite" ).addClass( "bgorange" ).text( "Preheat: On" );
+  } else {
+    $( "#preheatstatus" ).removeClass( "bgorange" ).addClass( "bgwhite" ).text( "Preheat: Off" );
+  }
+
+  var currentsetpoint = data.currentsetpoint.toFixed(1);
+  var override = data.override.toFixed(1);
+
+  if ( override > 0 ) {
+    currentsetpoint = currentsetpoint + "+" + override + "&deg;";
+  } else if ( override < 0 ) {
+    currentsetpoint = currentsetpoint + "-" + override + "&deg;";
+  } else {
+    currentsetpoint = currentsetpoint + "&deg;";
+  }
+  $( "#currentsetpoint" ).html( currentsetpoint );
+
+  $( "#override" ).html( "Override by " + override + "&deg;" );
+
+  if ( data.nextsetpoint ) {
+    $( "#nextsetpoint" ).html( data.nextsetpoint.toFixed(1) + "&deg;" );
+  } else {
+    $( "#nextsetpoint" ).text( "Unknown" ) ;
+  }
+
+  if ( data.nexteventstart ) {
+    var nexteventstart = new moment( data.nexteventstart );
+    $( "#nexteventstart" ).text( nexteventstart.format( "DD/MM/YYYY HH:mm" ));
+  } else {
+    $( "#nexteventstart" ).text( "Unknown" );
+  }
+
+  var goneOutUntil = "";
+  if ( data.goneoutuntil ) {
+    goneOutUntil = moment( data.goneoutuntil );
+    $( "#goneoutuntil" ).val( goneOutUntil.format( "HH:mm" ) );
+  } else {
+    $( "#goneoutuntil" ).val( "" );
+  }
+}
+
+$( document ).ready( function () {
+  $( "#decrease" ).click( function() {
+    overrideTemp -= 0.5;
+    $.ajax({
+      type: "GET",
+      url: "/heating/override/" + overrideTemp
+    })
+      .done( function () { getNewValues( true, false ) } );
+  });
+
+  $( "#increase" ).click( function() {
+    overrideTemp += 0.5;
+    $.ajax({
+      type: "GET",
+      url: "/heating/override/" + overrideTemp
+    })
+      .done( function () { getNewValues( true, false ) } );
+  });
+
+  $( "#goneoutuntil" ).clockTimePicker({
+    precision: 10,
+    afternoonHoursInOuterCircle: true,
+    minimum: moment().add( 1, 'hours' ).format( "HH:mm" ),
+    required: true,
+    onChange:( function( newValue, oldValue ) {
+      var newTime = moment();
+      var hours = newValue.substring( 0, 2 );
+      var minutes = newValue.substring( 3, 5 );
+      newTime.hour(hours);
+      newTime.minute(minutes);
+      newTime.second(0);
+      newTime.millisecond(0);
+      $.ajax({
+        type: "GET",
+        url: "/heating/gone_out_until/" + newTime.format()
+      })
+        .done( function () { getNewValues( true, false ) } )
+    })
+  });
+
+  $( "#clear" ).click( function() {
+    $( "#goneoutuntil" ).val( "" );
+    $.ajax({
+      type: "GET",
+      url: "/heating/gone_out_until/null"
+    })
+      .done( function () { getNewValues( true, false ) } )
+  });
+
+  getNewValues( true, true );
+});
