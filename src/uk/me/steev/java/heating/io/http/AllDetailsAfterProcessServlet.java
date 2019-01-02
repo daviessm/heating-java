9eb65919618db59a40fa8c435d80c0dc70392ec6
Steven Davies <github@steev.me.uk>
2019-01-02 14:35:56 +0000

    Use JSON NULL type

--- src/uk/me/steev/java/heating/io/http/AllDetailsAfterProcessServlet.java
+++ src/uk/me/steev/java/heating/io/http/AllDetailsAfterProcessServlet.java
@@ -84,7 +84,7 @@ public class AllDetailsAfterProcessServlet extends HeatingServlet {
 
     json.put("override", heating.getOverrideDegrees());
 
-    json.put("goneoutuntil", heating.getGoneOutUntilTime());
+    json.put("goneoutuntil", null == heating.getGoneOutUntilTime() ? JSONObject.NULL : heating.getGoneOutUntilTime());
     response.setStatus(HttpServletResponse.SC_OK);
     response.getWriter().println(json.toString(2));
   }
