a069f2f5be3398b2972d3535cf65f35bd18f7e5e
Steven Davies <github@steev.me.uk>
2019-12-12 11:18:53 +0000

    Remove unused import

--- src/uk/me/steev/java/heating/io/http/HttpAdapter.java
+++ src/uk/me/steev/java/heating/io/http/HttpAdapter.java
@@ -1,7 +1,5 @@
 package uk.me.steev.java.heating.io.http;
 
-import java.io.IOException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.CustomRequestLog;
